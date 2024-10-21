5f4668177a3fe93bc85949da042ff08b9c6c4509
Admin <anishkdevadiga2002@gmail.com>
2024-10-21 22:13:12 +0530

    added exit page

--- app/src/main/java/com/example/shuttlescore/FinishScreen.java
+++ app/src/main/java/com/example/shuttlescore/FinishScreen.java
@@ -1,9 +1,13 @@
 package com.example.shuttlescore;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -11,14 +15,19 @@ import androidx.core.view.WindowInsetsCompat;
 public class FinishScreen extends AppCompatActivity {
 
     @Override
+    @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_finish_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+         CardView c1=findViewById(R.id.backhome);
+         c1.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent intent=new Intent(FinishScreen.this,MainActivity.class);
+                 startActivity(intent);
+                 finish();
+             }
+         });
     }
 }
